2e9cebc084a27588f13e9ec38f2ddccbd0865c8b
JiangVN <157776686+JiangVN@users.noreply.github.com>
2024-01-27 21:59:33 +0700

    Delete Tut01 directory

--- Tut01/grayscale.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-def grayScale(input, output):
-    image = Image.open(input)
-    grayScale_image = image.convert('L')
-    grayScale_image.save(output)
-    print(f"GrayScale Image saved as {output}")
-
-grayScale('...........jpg', 'grayscale.jpg')
--- Tut01/practice.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-def loadImage(path):
-    image = Image.open(path)
-    image.show()
-
-loadImage('...........jpg')
